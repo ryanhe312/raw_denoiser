d61ca49b7b0262b6d75ea51d5bcef827e58e691e
ryanhe312 <312013477@qq.com>
2020-03-03 16:27:11 +0800

    fix bugs

--- Model_GRDN.py
+++ Model_GRDN.py
@@ -3,47 +3,49 @@ from keras.layers.merge import add
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dropout, PReLU,Activation, Conv2DTranspose,GlobalAveragePooling2D, GlobalMaxPooling2D, Reshape, Dense, multiply, Permute, Concatenate, Add, Activation, Lambda, Multiply
 from os import environ
 
-def Dense(filters):
-    def func(x):
-        out = Conv2D(filters = filters, \
+import keras.backend as K
+
+def DenseBlock(filters):
+	def func(x):
+		out = Conv2D(filters = filters, \
 					kernel_size=(3,3), \
 					strides=(1, 1), \
 					padding='same' , \
 					kernel_initializer='he_normal',\
 					activation='relu')(x)
-        out = Concatenate(axis = -1)([x, out])
-        return out
+		out = Concatenate(axis = -1)([x, out])
+		return out
 	return func
 
-def RDBlock(filters, growth_rate, dense_count = 3):
+def RDBlock(filters, growth_rate, dense_count):
 	def func(x):
 		out = x
 		
 		for i in range(dense_count):
-            out = Dense(filters = growth_rate)(out)
+			out = DenseBlock(filters = growth_rate)(out)
 
 		out = Conv2D(filters = filters, \
 				     kernel_size=(1,1), \
 					 strides=(1, 1), \
 					 padding='same', \
 					 kernel_initializer='he_normal',\
-					 activation='relu')(concate)
-		return feat
+					 activation='relu')(out)
+		return out
 	return func
 
-def GRDBlock(filters, growth_rate, rdb_count = 4, dense_count=3):
-    def func(x):
+def GRDBlock(filters, growth_rate, rdb_count, dense_count):
+	def func(x):
 		out = x
-		outputlist = []
+		output_list = []
 
 		for i in range(rdb_count):
 			output = RDBlock(filters = filters,\
 							 growth_rate = growth_rate, \
 							 dense_count = dense_count)(out)
-			share.append(output)
+			output_list.append(output)
 			out = output
 		
-		concate = Concatenate(axis = -1)(share)
+		concate = Concatenate(axis = -1)(output_list)
 		conv = Conv2D(filters = filters, \
 					  kernel_size=(1,1), \
 					  strides=(1, 1), \
@@ -56,24 +58,22 @@ def GRDBlock(filters, growth_rate, rdb_count = 4, dense_count=3):
 
 	return func
 
-def GGRDBlock(filters, growth_rate, grdb_count = 4, rdb_count = 4, dense_count=3):
-    def func(x):
-        output = x
-        for i in range(grdb_count):
-            output = GRDBlock(filters = filters, \
+def GGRDBlock(filters, growth_rate, grdb_count, rdb_count, dense_count):
+	def func(x):
+		output = x
+		for i in range(grdb_count):
+			output = GRDBlock(filters = filters, \
 							  growth_rate = growth_rate, \
 							  rdb_count = rdb_count,\
 							  dense_count = dense_count)(output)
-
 		out = Add()([output , x])
-        return out
-
+		return out
 	return func
 
 def ChannelAttention(ratio=8):
 	def func(x):
 		channel = x._keras_shape[-1]
-		
+
 		shared_layer_one = Dense(channel//ratio,\
 								activation='relu',\
 								kernel_initializer='he_normal',\
@@ -92,7 +92,7 @@ def ChannelAttention(ratio=8):
 		avg_pool = shared_layer_two(avg_pool)
 		assert avg_pool._keras_shape[1:] == (1,1,channel)
 		
-		max_pool = GlobalMaxPooling2D()(input_feature)
+		max_pool = GlobalMaxPooling2D()(x)
 		max_pool = Reshape((1,1,channel))(max_pool)
 		assert max_pool._keras_shape[1:] == (1,1,channel)
 		max_pool = shared_layer_one(max_pool)
@@ -140,28 +140,31 @@ def CBAMBlock(ratio=8):
 	return func
 
 def get_grdn(patch_size):
-    Inputs = Input((patch_size, patch_size, 4),name='input')
+	Inputs = Input((patch_size, patch_size, 4),name='input')
 
 	Conv1 = Conv2D(filters = 64, \
-					 kernel_size=(3,3), \
-					 strides=(1, 1), \
-					 padding='same' , \
-					 kernel_initializer='he_normal',\
-					 activation='relu')(Inputs)
-
-	Conv2 = Conv2D(filters = 64, \
+					kernel_size=(3,3), \
+					strides=(1, 1), \
+					padding='same' , \
+					kernel_initializer='he_normal',\
+					activation='relu')(Inputs)
+	
+	Conv2 = Conv2D(filters = 128, \
 					 kernel_size=(4,4), \
 					 strides=(2, 2), \
 					 padding='same' , \
 					 kernel_initializer='he_normal')(Conv1)
+	
 
-	Grdbs = GGRDBlock(128,128)(Conv2)
+	Grdbs = GGRDBlock(128,128,8,4,4)(Conv2)
 
-	Deconv2 = Conv2DTranspose(filters = 64, \,
+	
+	Deconv2 = Conv2DTranspose(filters = 128,\
 							kernel_size=(4,4), \
 							strides=(2, 2), \
 							padding='same' , \
 							kernel_initializer='he_normal')(Grdbs)
+	
 
 	Cbam = CBAMBlock()(Deconv2)
 
@@ -172,19 +175,19 @@ def get_grdn(patch_size):
 					 kernel_initializer='he_normal',\
 					 activation='relu')(Cbam)
 
-	Add = Add()([Inputs,Conv3])
+	Add1 = Add()([Inputs,Conv3])
 
-    model = Model(inputs=Inputs, outputs=Add)
+	model = Model(inputs=Inputs, outputs=Add1)
 
-    return model
+	return model
 
 
 def main():
-    environ["CUDA_VISIBLE_DEVICES"] = "2"
-    patch_size = 256
-    model=get_grdn(patch_size)
-    model.summary()
-    #model.save('./model-resnet/model-'+str(patch_size)+'.mdl')
+    #environ["CUDA_VISIBLE_DEVICES"] = "2"
+	patch_size = 256
+	model = get_grdn(patch_size)
+	model.summary()
+	model.save('./model-grdn/model-'+str(patch_size)+'.mdl')
 
 if __name__=='__main__':
     main()
